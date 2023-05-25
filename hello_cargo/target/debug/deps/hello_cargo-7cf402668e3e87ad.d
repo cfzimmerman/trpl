306fc5ea5294e1f2b8abcee3511b7ccdb214a474
rendergame <admin@render.game>
2023-05-25 13:18:16 -0500

    hello cargo

---
+++ hello_cargo/target/debug/deps/hello_cargo-7cf402668e3e87ad.d
@@ -0,0 +1,5 @@
+/Users/cory/Projects/trpl/hello_cargo/target/debug/deps/hello_cargo-7cf402668e3e87ad.rmeta: src/main.rs
+
+/Users/cory/Projects/trpl/hello_cargo/target/debug/deps/hello_cargo-7cf402668e3e87ad.d: src/main.rs
+
+src/main.rs:
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/cory/Projects/trpl/hello_cargo/target/debug/hello_cargo: /Users/cory/Projects/trpl/hello_cargo/src/main.rs
